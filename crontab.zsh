c7b33799628649ab53208d1797454372e4bb70bb
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2021-12-25 22:29:29 +0900

    add cron jedi

---
+++ crontab.zsh
@@ -0,0 +1,17 @@
+# crontab -l
+# http://d.hatena.ne.jp/anatoo/20120424/1335198888
+# * * * * * /bin/echo 'display notification "Sit up straight" with title "Cron Jedi"' | /usr/bin/osascript
+# * * * * * DISPLAY=:0.0 /usr/bin/notify-send "Cron Jedi" "Sit up straight"
+# http://stackoverflow.com/questions/610839/how-can-i-programmatically-create-a-new-cron-job
+case "$OSTYPE" in
+  darwin*)
+    (crontab -l ; echo '* * * * * /bin/echo '\''display notification "Sit up straight" with title "Cron Jedi"'\'' | /usr/bin/osascript') | sort - | uniq - | crontab -
+  ;;
+  linux*)
+    # If their is no crontab, then it will fail.
+    (crontab -l ; echo '* * * * * DISPLAY=:0.0 /usr/bin/notify-send "Cron Jedi" "Sit up straight"') | sort - | uniq - | crontab -
+  ;;
+  dragonfly*|freebsd*|netbsd*|openbsd*)
+    # ...
+  ;;
+esac
